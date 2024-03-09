e770a984244ad77aad075d43b5903b18950cea29
Ronaldo <r2desen@gmail.com>
2024-03-09 16:49:34 -0300

    Finished reading from MongoDB

--- netlify/functions/pets.js
+++ netlify/functions/pets.js
@@ -1,8 +1,16 @@
+const { MongoClient } = require("mongodb");
+
 const handler = async () => {
+  const client = new MongoClient(process.env.CONNECTIONSTRING);
+  await client.connect();
+
+  const pets = await client.db().collection("pets").find().toArray();
+  client.close();
+
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: "Hello There",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(pets),
   };
 };
 
